03d7400757a943f544a7610e7c8edde8a4cd90d9
Malcolm Almuntazar-Harris <almuntazar.harris.malcolm@gmail.com>
2019-04-19 20:34:34 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -8,31 +8,41 @@ import spotipy
 import spotipy.util as util
 import configparser
 
-def match_songtitle(title):
+reddit_timeframes = {
+        'a':'all',
+        'd':'day',
+        'h':'hour',
+        'm':'month',
+        'w':'week',
+        'y':'year',
+        }
+
+def song_stringer(title):
     """
     Find links with 'Artist - Song Title'
-    returns a tuple with ('artist','title')
+    returns a string with song and artist
     """
     split_list = title.split('-')
     if 'FRESH' in split_list[0]:
         bracketsplit = split_list[0].split(']')
         split_list[0] = bracketsplit[1]
-    return (split_list[0], split_list[1])
+    return f'{split_list[0]} {split_list[1]}'
 
 reddit = praw.Reddit(user_agent='Subreddit Playlist Creator (by /u/malcolm_exe)', client_id='kVTqgt1Y9v8dYQ',client_secret='M3Z0i5NFb8-RpOLjJXFjRlro9R0',username='malcolm_exe')
 
-#user input for subreddit
+#user input for subreddit and timeframe
 subreddit = reddit.subreddit(input('What subreddit? /r/: '))
+while True:
+    timeframe = input('What timeframe? Top of [a]ll, [d]ay, [h]our, [m]onth, [w]eek, or [y]ear?')
+    if timeframe in ('a','d','h','m','w','y'):
+        break
 
 #create list of links, filters by '-' to indicate song title
 songs = []
-for submission in subreddit.top(time_filter='year',limit=500):
+for submission in subreddit.top(time_filter=reddit_timeframes[timeframe],limit=500):
     if '-' in submission.title:
-        song_string = match_songtitle(submission.title)
-        song_string = f'{song_string[0]} {song_string[1]}'
+        song_string = song_stringer(submission.title)
         songs.append(song_string)
-#to-do: 1. pull trackid's from spotify
-#2. add 3 first tracks for albums
         
 #load configuration
 config = configparser.ConfigParser()
@@ -56,6 +66,7 @@ if token:
         if result['tracks']['total'] > 0:
             for r in result['tracks']['items']:
                 if(len(search_queries)<100):
+#100 limits the playlists to max 100 
                     search_queries.append(r['id'])
                     
 #clear all occurences before adding track ids
@@ -75,9 +86,7 @@ if token:
 
 
 #TO-DO
-#1. X have it clear everytime you run script. DONE
-#2. X remove duplicates before adding DONE
-#3. X more refined search DONE 
-#4. X user input for subreddit DONE
-#5. have it pull the track id if it's a spotify link
-#6. learn git and refine this?
+#This needs to do a few things differently
+#1. It'd be easier to search on reddit and return top tracks that way
+#2. Extract track ids from links instead
+#3. Update the description each time ran with time, date, subreddit, time
